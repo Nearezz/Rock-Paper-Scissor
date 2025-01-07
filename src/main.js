f7a973360d469b3b4e173b437362472bc1fc7c31
Nearez <97766579+Nearezz@users.noreply.github.com>
2025-01-07 15:42:32 -0500

    added button changing sign

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 const choices = document.querySelectorAll(".choice")
 const playerChoiceIcon = document.querySelector("#playerIcon")
+const computerChoiceIcon = document.querySelector("#computerIcon")
 
+const imgSrc = new Map([
+    [0,"http://127.0.0.1:5500/src/Images/paper.png"],
+    [1,"http://127.0.0.1:5500/src/Images/rock.png"],
+    [2,"http://127.0.0.1:5500/src/Images/scissors.png"]
+])
 
 choices.forEach((choice) => {
-    choice.addEventListener("click", (event) => {
+    choice.addEventListener("click", () => {
         const img = choice.querySelector("img")
-        console.log(img)
-
+        console.log(img.src)
         playerChoiceIcon.src = img.src
-
+        computerChoiceIcon.src = imgSrc.get(Math.floor(Math.random()*3))
 
     })
 })
