0f8a3e9d97c0b5a0373dd84f3ddd8c706deeaf1b
Nearez <97766579+Nearezz@users.noreply.github.com>
2025-01-08 00:16:43 -0500

    Added more features need to work on scoring mechnism and reply mechinism

--- src/main.js
+++ src/main.js
@@ -2,18 +2,74 @@ const choices = document.querySelectorAll(".choice")
 const playerChoiceIcon = document.querySelector("#playerIcon")
 const computerChoiceIcon = document.querySelector("#computerIcon")
 
+let PlayerScore = 0
+let computerScore = 0
+
+let playerAndComputerChoice = []
+
 const imgSrc = new Map([
-    [0,"http://127.0.0.1:5500/src/Images/paper.png"],
-    [1,"http://127.0.0.1:5500/src/Images/rock.png"],
-    [2,"http://127.0.0.1:5500/src/Images/scissors.png"]
+    ["paper","http://127.0.0.1:5500/src/Images/paper.png"],
+    ["rock","http://127.0.0.1:5500/src/Images/rock.png"],
+    ["scissors","http://127.0.0.1:5500/src/Images/scissors.png"]
 ])
+
+
+
+const winnerArray = [["rock","scissors"],["scissors","paper"],["paper","rock"]]
+
+
+function getComputerChoice() {
+    choicesForComputer = ["rock","paper","scissors"]
+    randomChoice = Math.floor(Math.random()*choicesForComputer.length)
+    return choicesForComputer[randomChoice]
+}
+
+
+const uIChangeMap = new Map([
+    ["player", () => {
+        const container = document.querySelector("#container");
+        const winnerTitle = document.querySelector("h1");
+        winnerTitle.textContent = "Player Wins";
+    }],
+    ["computer", () => {
+        const container = document.querySelector("#container");
+        const winnerTitle = document.querySelector("h1");
+        winnerTitle.textContent = "Computer Wins";
+    }],
+    ["draw", () => {
+        const container = document.querySelector("#container");
+        const winnerTitle = document.querySelector("h1");
+        winnerTitle.textContent = "Nobody Wins";
+    }],
+]);
+
+
+
+function roundWinner(choiceArr) {
+    console.log(
+        winnerArray.some((pair) => pair[0] === choiceArr[0] && pair[1] === choiceArr[1])
+    );
+    if (winnerArray.some((pair) => pair[0] === choiceArr[0] && pair[1] === choiceArr[1])) {
+        uIChangeMap.get("player")();
+    } else if (choiceArr[0] === choiceArr[1]) {
+        uIChangeMap.get("draw")();
+    } else {
+        uIChangeMap.get("computer")();
+    }
+}
+
+
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
+        choiceArray = []
         const img = choice.querySelector("img")
-        console.log(img.src)
+        const computerChoice = getComputerChoice()
+        const playerChoice = img.alt
         playerChoiceIcon.src = img.src
-        computerChoiceIcon.src = imgSrc.get(Math.floor(Math.random()*3))
+        computerChoiceIcon.src = imgSrc.get(computerChoice)
+        choiceArray.push(playerChoice,computerChoice)
+        roundWinner(choiceArray)
 
     })
 })
